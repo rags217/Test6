69c03c54c82dbd4ae577c7d8a17b6d7cdbfb14f9
rags217 <raghavendran.chandran7@gmail.com>
2018-11-23 12:28:13 +0000

    Initial check-in of module Test6

---
+++ Test6/App.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Test6
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new ContentPage { Content = new Label {Text = "Hello", VerticalOptions = LayoutOptions.Center, HorizontalOptions = LayoutOptions.Center }};
+        }
+    }
+}
